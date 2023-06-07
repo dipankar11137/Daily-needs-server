65f044c31dae51f96e0694474bba71ae3ac657e2
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-08 01:58:10 +0600

    booking filter by email

--- index.js
+++ index.js
@@ -24,25 +24,25 @@ async function run() {
   try {
     await client.connect();
     // console.log("database connect");
-    const jobCollection = client.db("dailyNeeds").collection("jobs");
-    const userCollection = client.db("dailyNeeds").collection("users");
+    const jobCollection = client.db('dailyNeeds').collection('jobs');
+    const userCollection = client.db('dailyNeeds').collection('users');
     const allServicesCollection = client
-      .db("dailyNeeds")
-      .collection("services");
+      .db('dailyNeeds')
+      .collection('services');
     const bookingCollection = client
-      .db("dailyNeeds")
-      .collection("bookingServices");
+      .db('dailyNeeds')
+      .collection('bookingServices');
 
-  //   // // // // // // // // // // // //
+    //   // // // // // // // // // // // //
 
     // // post User
-    app.post("/user", async (req, res) => {
+    app.post('/user', async (req, res) => {
       const newProduct = req.body;
       const result = await userCollection.insertOne(newProduct);
       res.send(result);
     });
     // get User
-    app.get("/users", async (req, res) => {
+    app.get('/users', async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const mainProducts = await cursor.toArray();
@@ -51,14 +51,14 @@ async function run() {
 
     //                    Jobs
     // get Jobs
-    app.get("/jobs", async (req, res) => {
+    app.get('/jobs', async (req, res) => {
       const query = {};
       const cursor = jobCollection.find(query);
       const jobs = await cursor.toArray();
       res.send(jobs);
     });
     // post Jobs
-    app.post("/jobs", async (req, res) => {
+    app.post('/jobs', async (req, res) => {
       const newProduct = req.body;
       const result = await jobCollection.insertOne(newProduct);
       res.send(result);
@@ -67,21 +67,21 @@ async function run() {
 
     // //                        ALl services
     // All Services Collection
-    app.post("/allServices", async (req, res) => {
+    app.post('/allServices', async (req, res) => {
       const newProduct = req.body;
       const result = await allServicesCollection.insertOne(newProduct);
       res.send(result);
     });
 
     // get all services
-    app.get("/allServices", async (req, res) => {
+    app.get('/allServices', async (req, res) => {
       const query = {};
       const cursor = allServicesCollection.find(query);
       const mainProducts = await cursor.toArray();
       res.send(mainProducts);
     });
     // all service filter by service category
-    app.get("/allServices/:service", async (req, res) => {
+    app.get('/allServices/:service', async (req, res) => {
       const service = req.params.service;
       const query = { service };
       const cursor = allServicesCollection.find(query);
@@ -89,39 +89,48 @@ async function run() {
       res.send(user);
     });
     // get all services by id
-    app.get("/allServicesId/:id", async (req, res) => {
+    app.get('/allServicesId/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const products = await allServicesCollection.findOne(query);
       res.send(products);
     });
     // // Delete one Service
-    app.delete("/allServices/:id", async (req, res) => {
+    app.delete('/allServices/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await allServicesCollection.deleteOne(query);
       res.send(result);
     });
     // post  book services
-    app.post("/bookService", async (req, res) => {
+    app.post('/bookService', async (req, res) => {
       const newProduct = req.body;
       const result = await bookingCollection.insertOne(newProduct);
       res.send(result);
     });
     // // get Book Service
-    app.get("/bookService", async (req, res) => {
+    app.get('/bookService', async (req, res) => {
       const query = {};
       const cursor = bookingCollection.find(query);
       const mainProducts = await cursor.toArray();
       res.send(mainProducts);
     });
-
-       app.delete('/bookService/:id', async (req, res) => {
-         const id = req.params.id;
-         const query = { _id: ObjectId(id) };
-         const result = await bookingCollection.deleteOne(query);
-         res.send(result);
-       });
+    // delete booking
+    app.delete('/bookService/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+    //
+    // all booking filter by email
+    app.get('/allBooking/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const cursor = bookingCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
